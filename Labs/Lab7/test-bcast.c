f1debb7b17e498ade32614d2a123e6cfb4bbe016
Matthew Mannix <m.mannix3@gmail.com>
2016-10-31 10:04:59 -0400

    Complete Lab7.

--- Labs/Lab7/test-bcast.c
+++ Labs/Lab7/test-bcast.c
@@ -4,36 +4,23 @@
 #define N 500000
 
 int main(int argc, char** argv) {
-  int rank, size;
+    int rank, size;
 
-  /* initialize MPI */
-  MPI_Init(&argc, &argv);
+    /* initialize MPI */
+    MPI_Init(&argc, &argv);
 
-  /* get the rank (process id) and size (number of processes) */
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+    /* get the rank (process id) and size (number of processes) */
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  /* have process 0 do many sends */
-  if (rank == 0) {
-    int i, j;
-    for (i = 0; i < N; i++) {
-      for (j = 1; j < size; j++) {
-        MPI_Send(&i, 1, MPI_INT, j, 0, MPI_COMM_WORLD);
-      }
-    }
-  }
-
-  /* have the rest receive that many values */
-  else {
+    /* have process 0 do many sends */
     int i;
     for (i = 0; i < N; i++) {
-      int value;
-      MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Bcast(&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
     }
-  }
 
-  /* quit MPI */
-  MPI_Finalize( );
-  return 0;
+    /* quit MPI */
+    MPI_Finalize( );
+    return 0;
 }
 

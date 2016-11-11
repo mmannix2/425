d6295209208f899a3b5a1696860d7ce171d4c8de
Matthew Mannix <mmannix2@mail.umw.edu>
2016-11-11 10:04:53 -0500

    Write CUDA version of collatz.c.

--- Labs/Lab9/collatz.cu
+++ Labs/Lab9/collatz.cu
@@ -1,11 +1,17 @@
+#include <unistd.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define N 50000
+#define CORES 50000
 
 /* run the collatz conjecture and return the number of steps */
-__global__ unsigned int collatz(unsigned int x) {
-    unsigned int step = 0;
+__global__ void collatz(unsigned int* step) {
+    //Set x to the initial value of step
+    unsigned int x = step[blockIdx.x];
+    //Reset step to 0
+    step[blockIdx.x] = 0;
 
     /* do the iterative process */
     while (x != 1) {
@@ -14,27 +20,39 @@ __global__ unsigned int collatz(unsigned int x) {
         } else {
             x = 3 * x + 1;
         }
-        step++;
+        step[blockIdx.x]++;
     }
-
-    return step;
 }
 
 int main( ) {
     /* store the number of steps for each number up to N */
-    unsigned int steps[N];
-
-    /* run the collatz conjecture on all N items */
-    unsigned int i;
-    for (i = 0; i < N; i++) {
-        steps[i] = collatz(i + 1);
+    unsigned int cpu_steps[N];
+    unsigned int* gpu_steps;
+    
+    /* allocate space on the GPU */
+    cudaMalloc((void**) &gpu_steps, N * sizeof(unsigned int));
+    
+    for(int i=0; i < N; i++) {
+        cpu_steps[i] = i+1;
     }
 
+    /* send gpu_steps to the GPU */
+    cudaMemcpy(gpu_steps, cpu_steps, N * sizeof(unsigned int), cudaMemcpyHostToDevice);
+    
+    /* run the collatz conjecture on all N items */
+    collatz<<<CORES, 1>>>(gpu_steps);
+    
+    /* send gpu_steps back to the CPU */
+    cudaMemcpy(cpu_steps, gpu_steps, N * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+    
+    /* free the memory on the GPU */
+    cudaFree(gpu_steps);
+
     /* find the largest */
-    unsigned int largest = steps[0], largest_i = 0;
-    for (i = 1; i < N; i++) {
-        if (steps[i] > largest) {
-            largest = steps[i];
+    unsigned int largest = cpu_steps[0], largest_i = 0;
+    for (int i = 1; i < N; i++) {
+        if (cpu_steps[i] > largest) {
+            largest = cpu_steps[i];
             largest_i = i;
         }
     }
